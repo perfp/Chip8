dbc44f5ffc9795374e718be0d0c781e79d796dbd
Per-Frode Pedersen <pfp@tuck.as>
2015-03-14 11:24:44 +0100

    Added sample programs

---
+++ c8/timertest.asm
@@ -0,0 +1,14 @@
+		CLS
+		LD I, #000
+		LD V3, #03
+		LD V4, #03
+		DRW V3, V4, 5
+		LD V0, #FF ; 4335 ms 
+		LD DT, V0
+LOOP:	LD V1, DT
+		SE V1, 00
+		JP LOOP
+		LD I, #005
+		DRW V3, V4, 5
+		RET
+
